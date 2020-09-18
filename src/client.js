0361f0b9e28ca4000b48c213836c76128bdcaf10
Imran Kheraj <ikheraj17@gmail.com>
2020-09-18 18:33:18 -0500

    main client.js server is set up properly

--- src/client.js
+++ src/client.js
@@ -1,4 +1,18 @@
 require("dotenv/config");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port =  process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+const app = express();
+const api = require('./main-service');
+app.use(cors());
+
+
+app.listen(port, host, async err => {
+    if(err) return console.error("there was an error connecting to the server: ", err);
+    console.log(`App listening on host "${host}" and port ${port}`);
+});
 /**
  * Example Client Service
  *
@@ -10,3 +24,4 @@ require("dotenv/config");
  * main service. This can be either an express server or another Node.js
  * framework if you have experience with others.
  */
+
